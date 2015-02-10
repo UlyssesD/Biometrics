f599c5f64ce16f3c7a8686cfcb9c72c67464938e
Stefano Di Biase <vanitas.sentiment.89@gmail.com>
2015-02-10 11:26:10 +0100

    Added OpenCV Framework

---
+++ Biometrics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Biometrics
+//
+//  Created by Ulysses D. on 10/02/15.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
