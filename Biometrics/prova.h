f26d9acadfee5cb50ea68c354cb8821f4bec6bc2
strongfunger <Marco@MacBook-Air-di-Marco.local>
2015-02-10 11:39:21 +0100

    just married

---
+++ Biometrics/prova.h
@@ -0,0 +1,14 @@
+//
+//  prova.h
+//  Biometrics
+//
+//  Created by Marco Spizzico on 10/02/15.
+//
+//
+
+#ifndef __Biometrics__prova__
+#define __Biometrics__prova__
+
+#include <stdio.h>
+
+#endif /* defined(__Biometrics__prova__) */
