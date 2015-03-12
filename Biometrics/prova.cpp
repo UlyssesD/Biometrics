8efaaa8829af173859f38b5f4c99919653759abd
Stefano Di Biase <vanitas.sentiment.89@gmail.com>
2015-03-12 17:00:54 +0100

    Adjusted Camera Resolution

--- Biometrics/prova.cpp
+++ Biometrics/prova.cpp
@@ -15,6 +15,7 @@
 #include "opencv2/objdetect/objdetect.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/opencv.hpp"
 
 #include <iostream>
 #include <stdio.h>
@@ -31,9 +32,11 @@ String eyes_cascade_name = "/usr/local/share/OpenCV/haarcascades/haarcascade_eye
 CascadeClassifier face_cascade;
 CascadeClassifier eyes_cascade;
 string window_name = "Capture - Face detection";
+string capture_window = "Grayscale Snapshot Taken";
 
 RNG rng(12345);
 
+void on_push(int state, void*) {}
 /**
  * @function main
  */
@@ -41,13 +44,15 @@ int main( void )
 {
   VideoCapture capture;
   Mat frame;
-
-  //-- 1. Load the cascade
+  
+   //-- 1. Load the cascade
   if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
   if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
 
   //-- 2. Read the video stream
   capture.open( -1 );
+    capture.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+    capture.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
   if( capture.isOpened() )
   {
     for(;;)
@@ -56,13 +61,25 @@ int main( void )
 
       //-- 3. Apply the classifier to the frame
       if( !frame.empty() )
-       { detectAndDisplay( frame ); }
+      {
+          
+          imshow(window_name, frame);
+          //detectAndDisplay( frame );
+      }
       else
        { printf(" --(!) No captured frame -- Break!"); break; }
 
       int c = waitKey(10);
-      if( (char)c == 'c' ) { break; }
-
+      if( (char)c == 'q' ) { break; }
+      if( (char)c == ' ') {
+          //imwrite("/Users/Ulysses_D/Desktop/capture.jpg",frame);
+          Mat frame_gray;
+          cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+          equalizeHist( frame_gray, frame_gray );
+          imshow(capture_window, frame_gray);
+          //imwrite("/Users/Ulysses_D/Desktop/capture_gray.jpg",frame_gray);
+          
+      }
     }
   }
   return 0;
@@ -93,13 +110,13 @@ void detectAndDisplay( Mat frame )
       {
          //-- Draw the face
          Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
-         ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2), 0, 0, 360, Scalar( 255, 0, 0 ), 2, 8, 0 );
+         ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2), 0, 0, 360, Scalar( 0, 0, 255 ), 2, 8, 0 );
 
          for( size_t j = 0; j < eyes.size(); j++ )
           { //-- Draw the eyes
             Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
             int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-            circle( frame, eye_center, radius, Scalar( 255, 0, 255 ), 3, 8, 0 );
+            circle( frame, eye_center, radius, Scalar( 0, 255, 0 ), 3, 8, 0 );
           }
        }
 
